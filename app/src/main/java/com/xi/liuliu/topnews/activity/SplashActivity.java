003c3d9b941c7460fa1540cf13aea22bfdb83203
zhangxiaobei <zhangxiaobei99@126.com>
2017-06-20 10:36:54 +0800

    增加splash页面

---
+++ app/src/main/java/com/xi/liuliu/topnews/activity/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.xi.liuliu.topnews.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+
+import com.xi.liuliu.topnews.R;
+
+public class SplashActivity extends AppCompatActivity {
+    private Handler mHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+
+    //禁止用返回键
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
