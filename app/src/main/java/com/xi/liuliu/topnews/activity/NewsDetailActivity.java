8aac2deb3c0ce895a7af6fe13a5a32701c0445c4
zhangxiaobei <zhangxiaobei99@126.com>
2017-06-21 18:22:31 +0800

    新闻详情页，完成ProgressBar加载功能

--- app/src/main/java/com/xi/liuliu/topnews/activity/NewsDetailActivity.java
+++ app/src/main/java/com/xi/liuliu/topnews/activity/NewsDetailActivity.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.xi.liuliu.topnews.R;
 
@@ -20,6 +22,7 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
     private ImageView mMore;
     private ImageView mMyFavourite;
     private ImageView mShare;
+    private ProgressBar mProgressBar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,11 +33,26 @@ public class NewsDetailActivity extends AppCompatActivity implements View.OnClic
 
     private void initView() {
         mWebView = (WebView) findViewById(R.id.web_view_news_detail);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
         mLeftGoBack = (ImageView) findViewById(R.id.left_back_icon);
         mMore = (ImageView) findViewById(R.id.news_detail_more_icon);
         mMyFavourite = (ImageView) findViewById(R.id.favorite_icon_news_detail);
         mShare = (ImageView) findViewById(R.id.share_icon_news_detail);
         mLeftGoBack.setOnClickListener(this);
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress == 100) {
+                    mProgressBar.setVisibility(View.GONE);
+                } else {
+                    if (mProgressBar.getVisibility() != View.VISIBLE) {
+                        mProgressBar.setVisibility(View.VISIBLE);
+                    }
+                    mProgressBar.setProgress(newProgress);
+                }
+                super.onProgressChanged(view, newProgress);
+            }
+        });
         loadNews(mWebView, getIntent().getStringExtra("news_url"));
     }
 
