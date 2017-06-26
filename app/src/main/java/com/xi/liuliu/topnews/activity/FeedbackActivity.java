21d339cc6f3968607adc34a3a6a3ced0099c997d
zhangxiaobei <zhangxiaobei99@126.com>
2017-06-26 11:49:59 +0800

    反馈界面布局更新

--- app/src/main/java/com/xi/liuliu/topnews/activity/FeedbackActivity.java
+++ app/src/main/java/com/xi/liuliu/topnews/activity/FeedbackActivity.java
@@ -1,11 +1,11 @@
 package com.xi.liuliu.topnews.activity;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.xi.liuliu.topnews.R;
@@ -18,19 +18,19 @@ public class FeedbackActivity extends AppCompatActivity implements View.OnClickL
     private EditText mFeedbackContent;
     private Button mGoBack;
     private TextView mSend;
+    private ImageView mFeedbackPic;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
         mFeedbackContent = (EditText) findViewById(R.id.activity_feedback_content);
-        Drawable drawable = getResources().getDrawable(R.drawable.feedback_camera_icon);
-        drawable.setBounds(0, 0, 50, 50);
-        mFeedbackContent.setCompoundDrawables(null, null, drawable, null);
         mGoBack = (Button) findViewById(R.id.activity_feedback_go_back);
         mGoBack.setOnClickListener(this);
         mSend = (TextView) findViewById(R.id.activity_feedback_send);
         mSend.setOnClickListener(this);
+        mFeedbackPic = (ImageView) findViewById(R.id.activity_feedback_pic);
+        mFeedbackPic.setOnClickListener(this);
     }
 
     @Override
@@ -40,6 +40,10 @@ public class FeedbackActivity extends AppCompatActivity implements View.OnClickL
 
                 break;
             case R.id.activity_feedback_send:
+
+                break;
+            case R.id.activity_feedback_pic:
+
 
                 break;
         }
