ab033f9902fd9c7d337bc8298011a1a4be744cba
zhangxiaobei <zhangxiaobei99@126.com>
2017-07-06 14:07:40 +0800

    BrokeNewsActivity加上WebView

--- app/src/main/java/com/xi/liuliu/topnews/activity/BrokeNewsActivity.java
+++ app/src/main/java/com/xi/liuliu/topnews/activity/BrokeNewsActivity.java
@@ -2,14 +2,23 @@ package com.xi.liuliu.topnews.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import com.xi.liuliu.topnews.R;
 
 public class BrokeNewsActivity extends AppCompatActivity {
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_broke_news);
+        initWebView();
+    }
+
+    private void initWebView() {
+        mWebView = (WebView) findViewById(R.id.web_view_activity_broke_news);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+
     }
 }
