f709f3c9a5cef901b8755089c77c0d66f3f7f3f8
zhangxiaobei <zhangxiaobei99@126.com>
2017-07-12 12:21:31 +0800

    自定义LiveMediaController，完成直播页功能

--- app/src/main/java/com/xi/liuliu/topnews/activity/LiveActivity.java
+++ app/src/main/java/com/xi/liuliu/topnews/activity/LiveActivity.java
@@ -1,36 +1,151 @@
 package com.xi.liuliu.topnews.activity;
 
-import android.net.Uri;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.xi.liuliu.topnews.R;
+import com.xi.liuliu.topnews.controller.LiveMediaController;
 
-import io.vov.vitamio.LibsChecker;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import io.vov.vitamio.MediaPlayer;
-import io.vov.vitamio.Vitamio;
-import io.vov.vitamio.widget.MediaController;
 import io.vov.vitamio.widget.VideoView;
 
 public class LiveActivity extends AppCompatActivity {
     private VideoView mVideoView;
+    private LiveMediaController mMediaController;
+    private static final int MESSAGE_TIME = 10010;
+    private static final int MESSAGE_BATTERY = 10011;
+    private LiveTimer mLiveTimer;
+    private static final String TAG = "LiveActivity";
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MESSAGE_TIME:
+                    mMediaController.setTime(msg.obj.toString());
+                    break;
+                case MESSAGE_BATTERY:
+                    mMediaController.setBattery(msg.obj.toString());
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (!LibsChecker.checkVitamioLibs(this)) {
+        int flag = WindowManager.LayoutParams.FLAG_FULLSCREEN;
+        Window window = LiveActivity.this.getWindow();
+        window.setFlags(flag, flag);
+        if (!io.vov.vitamio.LibsChecker.checkVitamioLibs(this))
             return;
-        }
         setContentView(R.layout.activity_live);
-        if (Vitamio.isInitialized(this)) {
-            mVideoView = (VideoView) findViewById(R.id.video_view_live_activity);
-            String url = getIntent().getStringExtra("live_url");
-            mVideoView.setVideoURI(Uri.parse(url));
-            mVideoView.setVideoQuality(MediaPlayer.VIDEOQUALITY_HIGH);
-            MediaController controller = new MediaController(this);
-            mVideoView.setMediaController(controller);
-            mVideoView.setBufferSize(10240);
-            mVideoView.requestFocus();
+        String liveUrl = getIntent().getStringExtra("live_url");
+        mVideoView = (VideoView) findViewById(R.id.video_view_live_activity);
+        mVideoView.setVideoPath(liveUrl);
+        mMediaController = new LiveMediaController(this, mVideoView, this);
+        mVideoView.setMediaController(mMediaController);
+        mVideoView.setBufferSize(5 * 1024 * 1024);
+        mVideoView.setVideoQuality(MediaPlayer.VIDEOQUALITY_HIGH);
+        mVideoView.setOnInfoListener(new MediaPlayer.OnInfoListener() {
+            @Override
+            public boolean onInfo(MediaPlayer mp, int what, int extra) {
+                switch (what) {
+                    case MediaPlayer.MEDIA_INFO_BUFFERING_START:
+
+                        mp.pause();
+                        break;
+                    case MediaPlayer.MEDIA_INFO_BUFFERING_END:
+
+                        mp.start();
+                        break;
+                    case MediaPlayer.MEDIA_INFO_DOWNLOAD_RATE_CHANGED:
+                        break;
+                }
+                return true;
+            }
+        });
+        mMediaController.show(5000);
+        mVideoView.requestFocus();
+        registerBoradcastReceiver();
+        if (mLiveTimer == null) {
+            mLiveTimer = new LiveTimer();
+        }
+        mLiveTimer.start();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        if (mVideoView != null) {
+            mVideoView.setVideoLayout(VideoView.VIDEO_LAYOUT_SCALE, 0);
+        }
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mLiveTimer != null && !mLiveTimer.isInterrupted()) {
+            mLiveTimer.interrupt();
+            mLiveTimer = null;
+            Log.i(TAG, "mLiveTimer interrupt");
+        }
+        try {
+            unregisterReceiver(batteryBroadcastReceiver);
+        } catch (IllegalArgumentException ex) {
+
+        }
+    }
+
+    private BroadcastReceiver batteryBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (Intent.ACTION_BATTERY_CHANGED.equals(intent.getAction())) {
+                int level = intent.getIntExtra("level", 0);
+                int scale = intent.getIntExtra("scale", 100);
+                Message msg = new Message();
+                msg.obj = (level * 100) / scale + "";
+                msg.what = MESSAGE_BATTERY;
+                mHandler.sendMessage(msg);
+            }
+        }
+    };
+
+    public void registerBoradcastReceiver() {
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(batteryBroadcastReceiver, intentFilter);
+
+    }
+
+    class LiveTimer extends Thread {
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            while (true) {
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+                String str = sdf.format(new Date());
+                Message msg = new Message();
+                msg.obj = str;
+                msg.what = MESSAGE_TIME;
+                mHandler.sendMessage(msg);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
