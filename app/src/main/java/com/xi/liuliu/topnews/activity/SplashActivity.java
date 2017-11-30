d5c00a54046964fcf00023ebb9af61ca676ac91b
zhangxiaobei <zhangxiaobei99@126.com>
2017-11-30 16:52:05 +0800

    SplashActivity添加全屏图片并设置动画

--- app/src/main/java/com/xi/liuliu/topnews/activity/SplashActivity.java
+++ app/src/main/java/com/xi/liuliu/topnews/activity/SplashActivity.java
@@ -5,26 +5,80 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.xi.liuliu.topnews.R;
 
+import java.util.Random;
+
 public class SplashActivity extends AppCompatActivity {
     private static final String TAG = "SplashActivity";
-    private Handler mHandler;
+    private ImageView mImageView;
+    private Animation mFadeIn;
+    private Animation mFadeScale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        mHandler = new Handler();
-        mHandler.postDelayed(new Runnable() {
+        initView();
+    }
+
+    private void initView() {
+        mImageView = (ImageView) findViewById(R.id.splash_activity_scene);
+        int index = new Random().nextInt(2);
+        if (index == 1) {
+            mImageView.setImageResource(R.drawable.splash_activity_entrance_1);
+        } else {
+            mImageView.setImageResource(R.drawable.splash_activity_entrance_2);
+        }
+        mFadeIn = AnimationUtils.loadAnimation(this, R.anim.welcome_fade_in);
+        mFadeIn.setAnimationListener(new Animation.AnimationListener() {
             @Override
-            public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+            public void onAnimationStart(Animation animation) {
+
             }
-        }, 2000);
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mImageView.startAnimation(mFadeScale);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        mFadeScale = AnimationUtils.loadAnimation(this, R.anim.welcome_fade_in_scale);
+        mFadeScale.setFillAfter(true);
+        mFadeScale.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }, 500);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        mImageView.startAnimation(mFadeIn);
+
     }
 
     //禁止用返回键
