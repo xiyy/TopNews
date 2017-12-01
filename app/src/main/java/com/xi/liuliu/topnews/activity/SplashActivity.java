937b30f01c46abe607f7a2d4326752cb0cf44711
zhangxiaobei <zhangxiaobei99@126.com>
2017-12-01 16:06:21 +0800

    fix SplashActivity animation

--- app/src/main/java/com/xi/liuliu/topnews/activity/SplashActivity.java
+++ app/src/main/java/com/xi/liuliu/topnews/activity/SplashActivity.java
@@ -5,10 +5,10 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
-import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.xi.liuliu.topnews.R;
 
@@ -17,34 +17,43 @@ import java.util.Random;
 public class SplashActivity extends AppCompatActivity {
     private static final String TAG = "SplashActivity";
     private ImageView mImageView;
+    private RelativeLayout mLogoLayout;
+    private Animation mFadeIn;
     private Animation mFadeScale;
-    private int mBgType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //全屏显示，不显示系统的状态栏（屏幕上方的电池，时间，WiFi等）
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        //nextInt(n),n>0,[0,n-1]之间的随机数
-        mBgType = new Random().nextInt(2);//生成0或者1
-        //在setContentView前，设置activity的主题
-        if (mBgType == 0) {
-            setTheme(R.style.splash_activity_theme_tower);
-        } else {
-            setTheme(R.style.splash_activity_theme_road);
-        }
         setContentView(R.layout.activity_splash);
         initView();
     }
 
     private void initView() {
         mImageView = (ImageView) findViewById(R.id.splash_activity_scene);
-        //设置背景图，背景图与setTheme()保持一致
-        if (mBgType == 0) {
+        mLogoLayout = (RelativeLayout) findViewById(R.id.splash_activity_rl_logo);
+        int random = new Random().nextInt(2);
+        if (random == 0) {
             mImageView.setImageResource(R.drawable.splash_activity_tower);
         } else {
             mImageView.setImageResource(R.drawable.splash_activity_road);
         }
+        mFadeIn = AnimationUtils.loadAnimation(this, R.anim.welcome_fade_in);
+        mFadeIn.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mImageView.startAnimation(mFadeScale);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
         mFadeScale = AnimationUtils.loadAnimation(this, R.anim.welcome_fade_in_scale);
         mFadeScale.setFillAfter(true);
         mFadeScale.setAnimationListener(new Animation.AnimationListener() {
@@ -71,8 +80,8 @@ public class SplashActivity extends AppCompatActivity {
 
             }
         });
-        mImageView.startAnimation(mFadeScale);
-
+        mImageView.startAnimation(mFadeIn);
+        mLogoLayout.startAnimation(mFadeIn);
     }
 
     //禁止用返回键
