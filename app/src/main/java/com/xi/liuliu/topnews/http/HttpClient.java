8c4b3f7f6213eebaf4e85ff10082a536768c3dea
zhangxiaobei <zhangxiaobei@sogou-inc.com>
2019-07-11 11:26:24 +0800

    让OkHttpClient保持单例，用同一个OkHttpClient实例来执行所有请求，因为每一个OkHttpClient实例都拥有自己的连接池和线程池，重用这些资源可以减少延时和节省资源。多次创建OkHttpClient会造成线程数过多

--- app/src/main/java/com/xi/liuliu/topnews/http/HttpClient.java
+++ app/src/main/java/com/xi/liuliu/topnews/http/HttpClient.java
@@ -20,12 +20,23 @@ import okhttp3.RequestBody;
 public class HttpClient {
     public static final String TAG = "HttpClient";
     private Callback mCallback;
+    private static OkHttpClient mOkHttpClient = null;
+
+    public static OkHttpClient getInstance() {
+        if (mOkHttpClient==null) {
+            synchronized (HttpClient.class) {
+                if (mOkHttpClient==null) {
+                    mOkHttpClient = new OkHttpClient();
+                }
+            }
+        }
+        return mOkHttpClient;
+    }
 
     public void requestNews(String channel) {
         if (!TextUtils.isEmpty(channel)) {
-            OkHttpClient okHttpClient = new OkHttpClient();
             Request request = new Request.Builder().get().url(Constants.HOST + "?type=" + channel + "&key=" + Constants.JU_HE_APP_KEY).build();
-            Call call = okHttpClient.newCall(request);
+            Call call = getInstance().newCall(request);
             //异步调用
             call.enqueue(mCallback);
         }
@@ -40,10 +51,9 @@ public class HttpClient {
         if (accessToken == null) {
             throw new NullPointerException("HttpClient.requestWeiboLogin(),accessToken is null ");
         }
-        OkHttpClient okHttpClient = new OkHttpClient();
         Request.Builder builder = new Request.Builder().get().url(Constants.WEI_BO_INFO_URL + "?access_token=" + accessToken.getToken() + "&uid=" + accessToken.getUid());
         Request request = builder.build();
-        Call call = okHttpClient.newCall(request);
+        Call call = getInstance().newCall(request);
         call.enqueue(mCallback);
     }
 
@@ -51,10 +61,9 @@ public class HttpClient {
         if (TextUtils.isEmpty(latitude) || TextUtils.isEmpty(longitude)) {
             throw new NullPointerException("HttpClient.requestAddresses(),latitude is null or longitude is null");
         }
-        OkHttpClient okHttpClient = new OkHttpClient();
         Request.Builder builder = new Request.Builder().get().url(Constants.GET_NEARBY_ADDRESSES_URL + "&location=" + latitude + "," + longitude + "&output=json&pois=1");
         Request request = builder.build();
-        Call call = okHttpClient.newCall(request);
+        Call call = getInstance().newCall(request);
         call.enqueue(mCallback);
     }
 
@@ -62,8 +71,9 @@ public class HttpClient {
         //表单提交
         RequestBody formBody = new FormBody.Builder().add("parameter", encryptGuid).build();
         Request request = new Request.Builder().url(Constants.IP_PANDA).addHeader("Content-Type", "application/x-www-form-urlencoded").post(formBody).build();
-        OkHttpClient okHttpClient = new OkHttpClient();
-        Call call = okHttpClient.newCall(request);
+        Call call = getInstance().newCall(request);
         call.enqueue(mCallback);
     }
+
+
 }
